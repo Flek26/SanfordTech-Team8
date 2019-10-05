17b6dec4f5852a5d9bec797b8df448357366c96b
Luke Fleck <33231394+Flek26@users.noreply.github.com>
2019-10-05 15:28:38 -0500

    toggle function

---
+++ SanfordTech/Assets/Scripts/Toggle.cs
@@ -0,0 +1,27 @@
+﻿//Attach this script to a Toggle GameObject. To do this, go to Create>UI>Toggle.
+//Set your own Text in the Inspector window
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Example : MonoBehaviour
+{
+    Toggle m_Toggle;
+    public GameObject m_obj;
+
+    void Start()
+    {
+        //Fetch the Toggle GameObject
+        m_Toggle = GetComponent<Toggle>();
+        //Add listener for when the state of the Toggle changes, and output the state
+        m_Toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(m_Toggle);
+        });
+    }
+
+    //Output the new state of the Toggle into Text when the user uses the Toggle
+    void ToggleValueChanged(Toggle change)
+    {
+        m_obj.SetActive(!m_obj.activeSelf); 
+    }
+}
---
+++ SanfordTech/Assets/Scripts/ToggleBodyParts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleBodyParts : MonoBehaviour
+{
+    Toggle m_Toggle;
+    public GameObject m_obj;
+
+    void Start()
+    {
+        //Fetch the Toggle GameObject
+        m_Toggle = GetComponent<Toggle>();
+        //Add listener for when the state of the Toggle changes, and output the state
+        m_Toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(m_Toggle);
+        });
+    }
+
+    //Output the new state of the Toggle into Text when the user uses the Toggle
+    void ToggleValueChanged(Toggle change)
+    {
+        m_obj.SetActive(!m_obj.activeSelf);
+    }
+}
